ddc4e72da3d15eb87defe21d45fad4b767a25a3c
Devyn <devynspillane@gmail.com>
2015-04-08 13:06:45 -0400

    Don't commit incomplete files... derp.

--- sql/dbconfig/create_tables.sql
+++ sql/dbconfig/create_tables.sql
@@ -22,10 +22,50 @@ CREATE TABLE papers (
 CREATE TABLE paper_authors (
 	paper_id	INTEGER NOT NULL,
     author_id	INTEGER NOT NULL,
-    PRIMARY KEY (paper_id, author_id)
+    PRIMARY KEY (paper_id, author_id),
+    CONSTRAINT fk_paper
+		FOREIGN KEY (paper_id)
+        REFERENCES papers(id)
+        ON DELETE CASCADE,
+	CONSTRAINT fk_author
+		FOREIGN KEY (author_id)
+        REFERENCES authors(id)
+        ON DELETE CASCADE
 );
 
 CREATE TABLE paper_citations (
 	citing_paper_id	INTEGER NOT NULL,
     cited_paper_id	INTEGER NOT NULL,
     PRIMARY KEY (citing_paper_id, cited_paper_id),
+    CONSTRAINT fk_citing_paper
+		FOREIGN KEY (citing_paper_id)
+        REFERENCES papers(id)
+        ON DELETE CASCADE,
+	CONSTRAINT fk_cited_paper
+		FOREIGN KEY (cited_paper_id)
+        REFERENCES papers(id)
+        ON DELETE CASCADE
+);
+
+CREATE TABLE keyphrases (
+	id		INTEGER NOT NULL AUTO_INCREMENT,
+	phrase	VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id),
+    INDEX idx_phrase (phrase),
+    UNIQUE (phrase)
+);
+
+CREATE TABLE paper_keyphrases (
+	paper_id		INTEGER NOT NULL,
+    keyphrase_id	INTEGER NOT NULL,
+    count			INTEGER NOT NULL,
+    PRIMARY KEY (paper_id, keyphrase_id),
+    CONSTRAINT fk_paper
+		FOREIGN KEY (paper_id)
+        REFERENCES papers(id)
+        ON DELETE CASCADE,
+	CONSTRAINT fk_keyphrase
+		FOREIGN KEY (keyphrase_id)
+        REFERENCES keyphrases(id)
+        ON DELETE CASCADE
+);
--- sql/dbconfig/drop_tables.sql
+++ sql/dbconfig/drop_tables.sql
@@ -1,3 +1,9 @@
+DROP TABLE IF EXISTS paper_keyphrases;
+
+DROP TABLE IF EXISTS keyphrases;
+
+DROP TABLE IF EXISTS paper_citations;
+
 DROP TABLE IF EXISTS paper_authors;
 
 DROP TABLE IF EXISTS papers;

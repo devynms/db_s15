5f11545681342913ede7302aa0516a75f4221f38
Devyn <devynspillane@gmail.com>
2015-04-09 10:56:07 -0400

    removed names for fk constraints

--- sql/dbconfig/create_tables.sql
+++ sql/dbconfig/create_tables.sql
@@ -33,8 +33,7 @@ CREATE TABLE releases (
     pubdate	DATE NOT NULL,
     publisher_name	VARCHAR (255) NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT fk_releases_publisher
-		FOREIGN KEY (publisher_name)
+    FOREIGN KEY (publisher_name)
         REFERENCES publishers(name)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
@@ -58,8 +57,7 @@ CREATE TABLE journals (
     volume		INTEGER NOT NULL,
     issue		INTEGER NOT NULL,
     PRIMARY KEY (release_id),
-    CONSTRAINT fk_journals_release
-		FOREIGN KEY (release_id)
+    FOREIGN KEY (release_id)
         REFERENCES releases(id)
         ON DELETE CASCADE
 );
@@ -69,12 +67,10 @@ CREATE TABLE conferences (
     location	TEXT NOT NULL,
     keynote_speaker	VARCHAR(255) NULL,
     PRIMARY KEY (release_id),
-    CONSTRAINT fk_conferences_release
-		FOREIGN KEY (release_id)
+    FOREIGN KEY (release_id)
         REFERENCES releases(id)
         ON DELETE CASCADE,
-	CONSTRAINT fk_conferences_keynote_speaker
-		FOREIGN KEY (keynote_speaker)
+	FOREIGN KEY (keynote_speaker)
         REFERENCES authors(name)
         ON DELETE SET NULL
         ON UPDATE CASCADE
@@ -88,8 +84,7 @@ CREATE TABLE papers (
     published_time	TIME,
     abstract		TEXT,
     PRIMARY KEY (id),
-    CONSTRAINT fk_papers_publisher
-		FOREIGN KEY (publisher_name)
+    FOREIGN KEY (publisher_name)
         REFERENCES publishers(name)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
@@ -112,12 +107,10 @@ CREATE TABLE paper_authors (
 	paper_id	INTEGER NOT NULL,
     author_name	VARCHAR(255) NOT NULL,
     PRIMARY KEY (paper_id, author_name),
-    CONSTRAINT fk_authors_paper
-		FOREIGN KEY (paper_id)
+    FOREIGN KEY (paper_id)
         REFERENCES papers(id)
         ON DELETE CASCADE,
-	CONSTRAINT fk_papers_author
-		FOREIGN KEY (author_name)
+	FOREIGN KEY (author_name)
         REFERENCES authors(name)
         ON DELETE RESTRICT
         ON UPDATE CASCADE
@@ -128,13 +121,10 @@ CREATE TABLE paper_citations (
     cited_paper_id	INTEGER NOT NULL,
     citation_text	TEXT NOT NULL,
     PRIMARY KEY (citing_paper_id, cited_paper_id),
-    CONSTRAINT fk_citing_paper
-		FOREIGN KEY (citing_paper_id)
+    FOREIGN KEY (citing_paper_id)
         REFERENCES papers(id)
         ON DELETE RESTRICT,
-	INDEX idx_cited_paper (cited_paper_id),
-	CONSTRAINT fk_cited_paper
-		FOREIGN KEY (cited_paper_id)
+	FOREIGN KEY (cited_paper_id)
         REFERENCES papers(id)
         ON DELETE RESTRICT
 );
@@ -149,12 +139,10 @@ CREATE TABLE paper_keyphrases (
     keyphrase	VARCHAR(255) NOT NULL,
     count		INTEGER NOT NULL,
     PRIMARY KEY (paper_id, keyphrase),
-    CONSTRAINT fk_keyphrases_paper
-		FOREIGN KEY (paper_id)
+    FOREIGN KEY (paper_id)
         REFERENCES papers(id)
         ON DELETE CASCADE,
-	CONSTRAINT fk_papers_keyphrase
-		FOREIGN KEY (keyphrase)
+	FOREIGN KEY (keyphrase)
         REFERENCES keyphrases(phrase)
         ON DELETE RESTRICT
 );
